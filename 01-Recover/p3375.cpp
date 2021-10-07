6dd279d07a129c4984365d9b1573e1705ffc6585
Odeinjul <odeinjul@gmail.com>
2021-10-07 21:17:22 +0800

    upt

--- 01-Recover/p3375.cpp
+++ 01-Recover/p3375.cpp
@@ -1,33 +1,27 @@
-#include<cstdio> 
-#include<cstring> 
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
 using namespace std;
 char a[1000100],b[1000100];
 int p[1000100];
-int main()
-{
-    scanf("%s%s",a+1,b+1);
-    /*for(int i = 0; i <= strlen(a+1); i++){
-        cout<<i<<' '<<a[i]<<endl;
-    }*/
+int main(){
+    scanf("%s%s", a+1, b+1);
     int la=strlen(a+1),lb=strlen(b+1);
-    p[1]=0;
-    int t = p[1];
+    int j=0;p[1]=0;
     for(int i = 2; i <= lb; i++){
-        while(t>0 && b[i]!=b[t+1]) t = p[t];
-        if(b[i]==b[t+1]) t++;
-        p[i]=t;
+        while(j != 0 && b[j+1] != b[i]) j=p[j];
+        if(b[j+1] == b[i]) j++; 
+        p[i]=j;
     }
-    t = 0;
+    j=0;
     for(int i = 1; i <= la; i++){
-        //t = p[i];
-        while(t>0 && a[i]!=b[t+1]) t = p[t];
-        if(a[i]==b[t+1]) t++;
-        if(t==lb){cout<<i-lb+1<<endl;}
+        while(j != 0 && b[j+1] != a[i]) j=p[j];
+        if(b[j+1] == a[i]) j++; 
+        if(j==lb) cout<<i-lb+1<<endl;
     }
-    for(int i=1;i<lb;i++)
+    for(int i = 1; i <= lb; i++){
         cout<<p[i]<<' ';
-    cout<<p[lb]<<endl;
+    }
+    cout<<endl;
     return 0;
 }
