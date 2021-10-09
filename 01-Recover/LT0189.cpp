ae1572edf384d820d828384fa8d5e93e3871b2e9
Odeinjul <odeinjul@gmail.com>
2021-10-09 23:05:11 +0800

    upd LT0189

--- 01-Recover/LT0189.cpp
+++ 01-Recover/LT0189.cpp
@@ -8,3 +8,20 @@ public:
         }
     }
 };
+/*
+    void rotate(vector<int>& nums, int k) 
+    {
+        k = nums.size() - k % nums.size();
+        trotate(nums, 0, k-1);
+        trotate(nums, k, nums.size()-1);
+        trotate(nums, 0, nums.size()-1);
+    }
+    void trotate(vector<int>& nums, int l, int r) 
+    {
+        while(l<r){
+            swap(nums[l], nums[r]);
+            l++; r--;
+        }
+    }
+    //luliyucoordinate
+*/
