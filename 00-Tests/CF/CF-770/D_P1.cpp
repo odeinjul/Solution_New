2e8b933e925302b1816bb2e70c2b0035372d8b79
Odeinjul <odeinjul@gmail.com>
2022-02-17 21:13:10 +0800

    Finished 1634D

--- 00-Tests/CF/CF-770/D_P1.cpp
+++ 00-Tests/CF/CF-770/D_P1.cpp
@@ -1,6 +1,8 @@
 //P means pass
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
+#include <cstdio>
 using namespace std;
 void work(int n);
 int maxn = -2;
@@ -11,65 +13,84 @@ int main(){
     for(int a = 0; a < t; a++){
         cin >> n;
         work(n);
+        fflush(stdout);
     }
+    fflush(stdout);
     return 0;
 }
 
 //cout.flush()
 
 void work(int n){
-    int flag1,flag2;
-    flag1 = help(2, n-1, n);
-    flag2 = help(flag1, n-1, n);
-    int a,b,c;
-    cout << "? " << 2 << ' ' << n-1 << ' ' << flag1 <<endl;
-    cout.flush();
-    cin >> a;
-    cout << "? " << flag2 << ' ' << 2 << ' ' << flag1 <<endl;
-    cout.flush();
-    cin >> b;
-    cout << "? " << 2 << ' ' << n-1 << ' ' << flag2 <<endl;
-    cout.flush();
-    cin >> c;
-    if(maxn == a){
-        cout<<"! "<<flag1<<' '<<n-1<<endl;
+    int flag1, flag2, temp;
+    int i = 1, j = 2;
+    maxn = -2; temp = 0;
+    for(int k = 3; k <= n; k++){
+        cout << "? " << i << ' ' << j << ' ' << k <<endl;
+        cout.flush();
+        cin>>temp;
+        if(temp >= maxn){
+            maxn = temp;
+            flag1 = k;
+        }
     }
-    else if(maxn == b){
-        cout<<"! "<<flag1<<' '<<n-1<<endl;
-    }
-    else if(maxn == c){
-        cout<<"! "<<n-1<<' '<<flag2<<endl;
-    }
-    else if(a == b){
-        cout<<"! "<<flag2<<' '<<flag1<<endl;
-    }
-    else if(a == c){
-        cout<<"! "<<flag2<<' '<<flag1<<endl;
-    }
-    else if(c == b){
-        cout<<"! "<<flag2<<' '<<n-1<<endl;
-    }
-    cout.flush();
-}
-
-int help(int i, int j, int n){
-    int temp = 0, flag = 0;
-    maxn = -2;
-    int cnt = 0;
-    for(int k = 1; k <= n; k++){
-        if(k == i || k == j)   
+    maxn = -2; temp = 0;
+    j = flag1;
+    for(int k = 3; k <= n; k++){
+        if(k == flag1)
             continue;
         cout << "? " << i << ' ' << j << ' ' << k <<endl;
         cout.flush();
-        cin >> temp;
-        if(temp == maxn){
-            cnt ++;
-        }
-        if(temp > maxn){
+        cin>>temp;
+        if(temp >= maxn){
             maxn = temp;
-            flag = k;
+            flag2 = k;
         }
     }
-    //cout<<cnt<<' '<<i<<' '<<endl;
-    return flag;//temp;
+    int a = 1, b = flag1, c = flag2;
+    int d = 2;
+    int aa = a, bb = b, cc = c;
+    while(d == a || d == b || d == c){
+        d++;
+    }
+    a = d;
+    int temp1,temp2,temp3;
+    cout << "? " << a << ' ' << b << ' ' << c <<endl;
+    cin >> temp1 ;
+    a = aa;
+    b = d;
+    cout << "? " << a << ' ' << b << ' ' << c <<endl;
+    cin >> temp2 ;
+    b = bb;
+    c = d;
+    cout << "? " << a << ' ' << b << ' ' << c <<endl;
+    cin >> temp3 ;
+    c = cc;
+    //cout<< temp1 << ' ' << temp2 << ' ' <<temp3 << ' '<<maxn<<endl;
+    if(temp1 == maxn && temp1 > temp2 && temp1 > temp3){
+        cout << "! " << b << ' '<< c << endl;
+        return ;
+    }
+    else if(temp2 == maxn && temp2 > temp1 && temp2 >temp3){
+        cout << "! " << a << ' '<< c << endl;
+        return ;
+    }
+    else if(temp3 == maxn && temp3 > temp1 && temp3 >temp2){
+        cout << "! " << a << ' '<< b << endl;
+        return ;
+    }
+    else if(temp1 == temp3){
+        cout << "! " << b << ' '<< d << endl;
+        return ;
+    }
+    else if(temp2 == temp3){
+        cout << "! " << a << ' '<< d << endl;
+        return ;
+    }
+    else if(temp2 == temp1){
+        cout << "! " << c << ' '<< d << endl;
+        return ;
+    }
+    return ;
+
 }
